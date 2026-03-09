e8de327ff9e169bb4836a0fe1508089d3517b8b1
kaiekaie <magnusaga@gmail.com>
2026-03-09 15:49:39 +0100

    added gradle scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,17 @@
+plugins {
+    id("com.gradle.develocity") version "4.3.2"
+}
+
 rootProject.name = "test-framework"
 
 include("java-library")
 include("plugins")
 include("java-spring-boot-sample-app")
 include("kotlin-spring-boot-sample-app")
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
+}
